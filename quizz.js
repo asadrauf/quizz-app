aa2d7e127231bfe50327056bb66a00718c2a0224
ARauf Git <umiasad2017@gmail.com>
2020-01-25 09:24:38 -0500

    version 1.1

--- quizz.js
+++ quizz.js
@@ -1,25 +1,25 @@
 // select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const heading = document.getElementById("heading");
-const para = document.getElementById("para");
-const question = document.getElementById("question");
-const span = document.getElementById("span");
-const submitButton = document.getElementById("btn");
-const textBox = document.getElementById("textArea");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
-const todDiv = document.getElementById("todo-form");
+var start = document.getElementById("start");
+var quiz = document.getElementById("quiz");
+var heading = document.getElementById("heading");
+var para = document.getElementById("para");
+var question = document.getElementById("question");
+var span = document.getElementById("span");
+var submitButton = document.getElementById("btn");
+var textBox = document.getElementById("textArea");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+var progress = document.getElementById("progress");
+var scoreDiv = document.getElementById("scoreContainer");
+var todDiv = document.getElementById("todo-form");
+var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
-var scoreList = document.querySelector("#todo-list");
 
 // create our questions
-let questions = [
+var questions = [
     {
         question : "What does HTML stand for?",
         choiceA : "Hyper Text Markup Language",
@@ -55,19 +55,19 @@ let questions = [
 
 // create some variables
 
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 75;
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
+var count = 75;
 //var quizTime ; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / count;
-let TIMER;
-let score = 0;
+var gaugeWidth = 150; // 150px
+var gaugeUnit = gaugeWidth / count;
+var TIMER;
+var score = 0;
 
 // render a question
 function renderQuestion(){
 
-    let q = questions[runningQuestion];
+    var q = questions[runningQuestion];
     question.innerHTML = "<p>"+ q.question +"</p>";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
@@ -165,6 +165,8 @@ function answerIsWrong(){
 }
 
 //score render
+var todos=[];
+var todo = [];
 function scoreRender(){
     quiz.style.display = "none";
     scoreDiv.style.display = "block";
@@ -172,19 +174,51 @@ function scoreRender(){
     textBox.style.display = "block";
     span.style.display = "block";
     submitButton.style.display = "block";
-    localStorage.setItem("finalScore", score);
-  
-}
-
-function passValue(){
-    var name = textBox.value.trim();
-    if(name === ""){
-        return;
-    }
-   // var score = scoreDiv.value;
-    //localStorage.setItem("finalScore", score);
-    localStorage.setItem("textValue", name);
-    return false;
+    //localStorage.setItem("finalScore", JSON.stringify(score));
    
 }
 
+// function passValue(){
+//     var name = textBox.value.trim();
+    
+//     //var score = scoreDiv.value;
+//     //localStorage.setItem("finalScore", score);
+//     localStorage.setItem("textValue", JSON.stringify (name));
+//     return false;
+   
+// }
+function renderTodos(){
+
+var length = todos.length;
+todoCountSpan.textContent = length;
+
+todoList.innerHTML = "";
+
+for (var i =0; i<length; i++){
+  var li = document.createElement("li");
+  li.textContent = todos[i];
+  li.setAttribute("data-index", i);
+  var button = document.createElement("button");
+  button.textContent = "Complete";
+  li.appendChild(button);
+  todoList.appendChild(li);
+}}
+
+function passValue(){
+var todotext = textBox.value;
+if(todotext === ""){
+  return;
+}
+todos.push(todotext);
+localStorage.setItem("todos", JSON.stringify(todos));
+todoInput.value = "";
+
+var scoretext = scoreDiv.value;
+if(scoretext === ""){
+  return;
+}
+todo.push(scoretext);
+localStorage.setItem("todo", JSON.stringify("todo"));
+todoInput.value = "";
+renderTodos();
+}
