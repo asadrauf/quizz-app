190c8e38bf4bcf1d84aeef857a9a1699bf155dba
ARauf Git <umiasad2017@gmail.com>
2020-01-27 17:17:45 -0500

    updated final version

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# quizz-app
+### Quizz App
+In this assignment, i am going to use fundamental HTML, CSS, and JavaScript skills to build a quiz application. This application will be able to load questions from a 3rd an object array, track and display high scores, and so much more! I am using HTML5 Local Storage to store my user score and username that will be displaying in the view highscore page
+
+* * Completed Tasks 
+    * [x] Local Storage 
+    * [x] Dsiplaying feedback for correct incorrect answers
+    * [x] JSON parse and strigify
+    * [x] Resposive for 768px Device
+    * [x] Resposive for 980px Device
+    * [x] Resposive for 375px Device
+    * [x] Using addEventListner strategy
+    * [x] Used objects array
+    * [x] Used If else conditions
+    * [x] Good naming convention practice
+    * [x] Spacing and indentation practice
+
+
+
+    * Media Queries Example
+
+    ```css
+      @media (min-width: 576px) { ... }
+    ```
+
+    ```css
+      @media (min-width: 992px) { ... }
+    ```
+
+    * Licence and copyright
+  
+    * Asad Rauf, Bootcamp Student
+
+    * Contact Info    
+
+    | Name      | Email                 |
+    | --------- | --------------------- |
+    | Asad Rauf | umiasad2017@gmail.com |
+
+
+
+
--- quizz.js
+++ quizz.js
@@ -1,10 +1,11 @@
-// select all elements
+
+
+/*-----------------selecting all DOM Elements-----------------------*/
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var heading = document.getElementById("heading");
 var para = document.getElementById("para");
 var question = document.getElementById("question");
-var span = document.getElementById("span");
 var submitButton = document.getElementById("btn");
 var textBox = document.getElementById("textArea");
 var choiceA = document.getElementById("A");
@@ -12,13 +13,13 @@ var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var counter = document.getElementById("counter");
 var timeGauge = document.getElementById("timeGauge");
-var progress = document.getElementById("progress");
 var scoreDiv = document.getElementById("scoreContainer");
-var todDiv = document.getElementById("todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
 
-// create our questions
+
+
+
+
+/*-----------------Creating our Questions Array object-----------------------*/
 var questions = [
     {
         question : "What does HTML stand for?",
@@ -30,11 +31,11 @@ var questions = [
         question : "What does CSS stand for?",
         choiceA : "Common Script Source",
         choiceB : "Cascading Style Sheets",
-        choiceC : "JQuery",
+        choiceC : "JQuery Styling sheet",
         correct : "B"
     },{
         question : "What does JS stand for?",
-        choiceA : "JQuery",
+        choiceA : "JQuery Script",
         choiceB : "Java Enterprise Application",
         choiceC : "Java Script",
         correct : "C"
@@ -45,20 +46,19 @@ var questions = [
         choiceC : "Java Script",
         correct : "A"
     },{
-        question : "What does JS stand for?",
-        choiceA : "JQuery",
+        question : "What does CDN stand for?",
+        choiceA : "JQuery Delivery Newtwork",
         choiceB : "Java Enterprise Application",
-        choiceC : "Java Script",
+        choiceC : "Content Delivery Network",
         correct : "C"
     }
 ];
 
-// create some variables
+/*-----------------Creating variables-----------------------*/
 
 var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
 var count = 75;
-//var quizTime ; // 10s
 var gaugeWidth = 150; // 150px
 var gaugeUnit = gaugeWidth / count;
 var TIMER;
@@ -76,155 +76,131 @@ function renderQuestion(){
 
 start.addEventListener("click",startQuiz);
 
-// start quiz
+/*-----------------Fuction that will start the quizz and will display the desireable section to the user-----------------------*/
 function startQuiz(){
     start.style.display = "none";
     renderQuestion();
     heading.style.display = "none";
     para.style.display = "none";
     quiz.style.display = "block";
-    //renderProgress();
     renderCounter();
     TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
 }
 
-// counter render
+/*-----------------Fuction that will update the count and guage unit based on answers-----------------------*/
 
 function renderCounter(){
-    if(count >= 0){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count--;
-    }else{
-        // change progress color to red
-        answerIsWrong();
-        count = count -10;
+        if(count >= 0){
+            counter.innerHTML = count;
+            timeGauge.style.width = count * gaugeUnit + "px";
+            count--;
+       }
+       else{
+        quiz.style.display = "none";
+        scoreDiv.style.display = "block";
+        scoreDiv.innerHTML = "Your Final Score is " + score;
+        textBox.style.display = "block";
+        span.style.display = "block";
+        submitButton.style.display = "block";
+
         if(runningQuestion < lastQuestion){
-            answerIsCorrect()
             runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
+        }
+        else{
             clearInterval(TIMER);
             scoreRender();
         }
     }
-}
+  }
 
-// checkAnwer
+/*-----------------Checking user selected answer-----------------------*/
 
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
         // answer is correct
-        
         score++;
         console.log(score);
-        // change progress color to green
         answerIsCorrect();
     }else{
         // answer is wrong
-        // change progress color to red
         count = count - 10;
         answerIsWrong();
     }
-    //count = 0;
+    /*-----------------Rendering questions-----------------------*/
     if(runningQuestion < lastQuestion){
         setTimeout(function(){
             runningQuestion++;  
             renderQuestion();
         },1000);
-        //runningQuestion++;
-       // renderQuestion();
-    }else{
+        
+    }
+    else{
         // end the quiz and show the score
         clearInterval(TIMER);
         scoreRender();
     }
-}
+  }
 
-// answer is correct
-function answerIsCorrect(){
-    //document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-    var element = document.getElementById("correct");
-    element.style.display = "block";
-    setTimeout(function(){
-        element.style.display = "none";
-    }, 1000);
-       
+/*-----------------Based on correct answer displaying message-----------------------*/
+    function answerIsCorrect(){
+        var element = document.getElementById("correct");
+        element.style.display = "block";
+        setTimeout(function(){
+            element.style.display = "none";
+        }, 1000);
+        
 
-}
+    }
 
-// answer is Wrong
-function answerIsWrong(){
-    //document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-    var element = document.getElementById("incorrect");
-    element.style.display = "block";
-    setTimeout(function(){
-        element.style.display = "none";
-    }, 1000);
-}
+/*-----------------Based on incorrect answer displaying message-----------------------*/
+    function answerIsWrong(){
 
-//score render
-var todos=[];
-var todo = [];
-function scoreRender(){
-    setTimeout(function() {
+        var element = document.getElementById("incorrect");
+        element.style.display = "block";
+        setTimeout(function(){
+            element.style.display = "none";
+        }, 1000);
+    }
+
+   /*-----------------Displaying the total score-----------------------*/
+    let todo = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
+    function scoreRender(){
+        setTimeout(function() {
         quiz.style.display = "none";
-    scoreDiv.style.display = "block";
-    scoreDiv.innerHTML = "Your Final Score is " + score;
-    textBox.style.display = "block";
-    span.style.display = "block";
-    submitButton.style.display = "block";
-    //localStorage.setItem("finalScore", JSON.stringify(score));
-    },1000)
+        scoreDiv.style.display = "block";
+        scoreDiv.innerHTML = " your score is " + score ;
+        textBox.style.display = "block";
+        span.style.display = "block";
+        submitButton.style.display = "block";
+        },1000)
+        
     
-   
-}
+    }
+/*-----------------Function that stores score and username to local storage-----------------------*/
+        function passValue(){
+        var todotext = textBox.value.trim();
+        if(todotext === ""){
+        return;
+        }
+        todo.push(todotext);
+        localStorage.setItem("todo" , JSON.stringify(todo));
 
-// function passValue(){
-//     var name = textBox.value.trim();
+        var scoretext = scoreDiv.textContent;
+        if(scoretext === ""){
+        return;
+        }
+        todo.push(scoretext);
+        localStorage.setItem("todo", JSON.stringify(todo));
+        
+        }
+
     
-//     //var score = scoreDiv.value;
-//     //localStorage.setItem("finalScore", score);
-//     localStorage.setItem("textValue", JSON.stringify (name));
-//     return false;
-   
-// }
-function renderTodos(){
-
-var length = todos.length;
-todoCountSpan.textContent = length;
-
-todoList.innerHTML = "";
-
-for (var i =0; i<length; i++){
-  var li = document.createElement("li");
-  li.textContent = todos[i];
-  li.setAttribute("data-index", i);
-  var button = document.createElement("button");
-  button.textContent = "Complete";
-  li.appendChild(button);
-  todoList.appendChild(li);
-}}
-
-function passValue(){
-var todotext = textBox.value;
-if(todotext === ""){
-  return;
-}
-todos.push(todotext);
-localStorage.setItem("todos", JSON.stringify(todos));
-//todoInput.value = "";
-
-var scoretext = scoreDiv.textContent;
-if(scoretext === ""){
-  return;
-}
-todo.push(scoretext);
-localStorage.setItem("todo", JSON.stringify(todo));
-//todoInput.value = "";
-//renderTodos();
-}
-
-//clear all values from local storage
-
+            
+            
+        
+        
+       
+        
+      
+        
+    
